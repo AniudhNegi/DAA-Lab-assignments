60d2fe30ce2dc5e676e96bdd5e346994339c8412
Anirudh Negi <69972736+AniudhNegi@users.noreply.github.com>
2021-09-22 21:52:29 +0530

    Update SelectionS.java

--- Week3/SelectionS.java
+++ Week3/SelectionS.java
@@ -4,48 +4,53 @@ import java.util.Scanner;
 
 public class SelectionS {
 	
-	public static void sort(int arr[]) {
+	public static void selectionS(int arr[]) {
+		
+		int cmp = 0;
+		int swap = 0;
 		for(int i =0;i<arr.length - 1;i++) {//0 to n-1 iterations 
 			int temp = arr[i];
 			int tempidx = i;
 			// searching for minimum element
 			for(int j =i+1 ;j<arr.length;j++) {
+				cmp++;
 				if(arr[j]<temp) {
 					temp = arr[j];
-					tempidx = j;
+					tempidx = j;					
 				}
 			}
-			//swap
+			swap++;
+			
 			if(tempidx != i) {
 				arr[tempidx] = arr[i];
 				arr[i] = temp;
 			}
 		}
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i] +" ");			
+		}
+		System.out.println("");
+		System.out.println("comparistion = " + cmp);
+		System.out.println("swap = " + swap);
+		
 		
 	}
-	 static void print(int arr[]){
-	        
-	    	int n =arr.length;
-	        
-	    	for (int i=0;i<n;i++){
-	        	
-	            System.out.print(arr[i] + " ");
-	        }
-	 }
-	 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for (int i = 0; i<n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		sort(arr);
-		System.out.println("Sorted Array:");
-		print(arr);
+		int t = sc.nextInt();
 		
-
+		for(int i = 0; i <= t; i++) {
+			int s = sc.nextInt();
+			int arr[] = new int[s];
+			
+			for(int j =0 ; j< s; j++) { 
+				arr[j] = sc.nextInt();
+				
+			}	
+			selectionS(arr);
+			System.out.println("");
+		}
 	}
 
 }
