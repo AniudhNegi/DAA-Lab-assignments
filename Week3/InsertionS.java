a2e924b22ae40e6ed9e6f6c0f490bfc9b7b5201e
Anirudh Negi <69972736+AniudhNegi@users.noreply.github.com>
2021-09-22 21:26:02 +0530

    Update InsertionS.java

--- Week3/InsertionS.java
+++ Week3/InsertionS.java
@@ -3,41 +3,48 @@ import java.util.Scanner;
 
 public class InsertionS {
 	
-	public static void insertion(int arr[]) {
-		for(int i=1;i<arr.length;i++) {//Unsorted array
+	public static void insertionS(int arr[]) {
+		int cmp = 0 , shift = 0;
+		
+        for(int i=1; i<arr.length; ++i) {//Unsorted array
 			
 			int temp =arr[i];
 			int j = i-1;
 			
-			while(j>=0 && arr[j]>temp) {
+			while(j>=0 && arr[j]>temp ) {
 				//Swap
+				cmp++;
+				++shift;
 				arr[j+1] = arr[j];
-				j--;
+				--j;
 			}
 			arr[j+1] = temp;
-			
+			++shift;
 		}
-	}
-	public static void print(int arr[]) {
+		
 		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i] +" ");
+			System.out.print(arr[i] +" ");			
 		}
-	}
+		System.out.println("");
+		System.out.println("comparistion = " + cmp);
+		System.out.println("shifts = " + shift);
+		
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
 		
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = sc.nextInt();
+		for(int i = 0; i < t; i++) {
+			int s = sc.nextInt();
+			int arr[] = new int[s];
+			
+			for(int j =0 ; j< s; j++) { 
+				arr[j] = sc.nextInt();
+				
+			}	
+			insertionS(arr);
+			System.out.println("");
 		}
-		insertion(arr);
-		System.out.println("Sorted Array:");
-		print(arr);
-		
-	}
 
 }
